270b70042af5e412661f33b33348b56fe9819095
sthsurendra <38010470+sthsurendra@users.noreply.github.com>
2020-03-30 19:14:55 -0500

    working fine addcart project

---
+++ WebContent/index.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+<form action="AddCartServlet" method="post">
+<input type="text" name="add">
+<input type="submit" value="submit">
+</form>
+</body>
+</html>
---
+++ src/AddCartServlet.java
@@ -0,0 +1,47 @@
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/AddCartServlet")
+public class AddCartServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    public AddCartServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String prod = request.getParameter("add");
+		
+		ArrayList<String> productList;
+		//System.out.println(items);
+		HttpSession session = request.getSession();
+		//session.setAttribute("shoppingiteam", prod);
+		Object o = session.getAttribute("cart");
+		if(o==null) {
+			productList = new ArrayList<String>();
+			productList.add(prod);
+			session.setAttribute("cart",productList);
+			System.out.println(productList);
+		}else {
+			ArrayList<String> existiteam = (ArrayList<String>)o;
+			existiteam.add(prod);
+			System.out.println(existiteam);
+		}
+		
+	}
+
+
+}
