2dcc3fd24bb47b1f37313b2354b9b16b3201ba59
sthsurendra <38010470+sthsurendra@users.noreply.github.com>
2020-03-30 19:19:09 -0500

    final project

--- src/AddCartServlet.java
+++ src/AddCartServlet.java
@@ -14,7 +14,7 @@ public class AddCartServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
     public AddCartServlet() {
         super();
-        // TODO Auto-generated constructor stub
+        
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
